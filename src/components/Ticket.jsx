007c17fe75303435f9b6ed0fb3169888931b8894
Student <example@example.com>
2019-07-01 08:22:57 -0700

    Initial build of help queue

---
+++ src/components/Ticket.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Ticket() {
+  return (
+    <div>
+      <h3>3A - Tessa</h3>
+      <p><em>Firebase entries not saving!</em></p>
+      <hr/>      
+    </div>
+  )
+}
+export default Ticket;
