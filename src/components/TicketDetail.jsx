388c21075e29aaf35362769af3289299132795ff
Student <example@example.com>
2019-07-10 13:25:57 -0700

    Destructured props argument in TicketDetail.jsx

--- src/components/TicketDetail.jsx
+++ src/components/TicketDetail.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const TicketDetail = (props) => {
+const TicketDetail = ({selectedTicket}) => {
   return (
     <div>
-      <h1>{props.selectedTicket.names} - {props.selectedTicket.location} </h1>
-      <h2>Submitted {props.selectedTicket.formattedWaitTime} ago</h2>
-      <h4><em>{props.selectedTicket.issue}</em></h4>
+      <h1>{selectedTicket.names} - {selectedTicket.location} </h1>
+      <h2>Submitted {selectedTicket.formattedWaitTime} ago</h2>
+      <h4><em>{selectedTicket.issue}</em></h4>
       
     </div>
   );
